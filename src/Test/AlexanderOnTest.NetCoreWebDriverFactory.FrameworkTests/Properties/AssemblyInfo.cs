d5f7e36416e5fc39d30bad57406d9ac98bab1783
Alexander Dunn <alexander@alexanderontesting.com>
2018-12-11 07:29:09 +0000

    2.0.0-beta1 release tidy up
    copyright and nuget tidy up

---
+++ src/Test/AlexanderOnTest.NetCoreWebDriverFactory.FrameworkTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FrameworkTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FrameworkTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3895788d-81bd-4818-9aa9-2bfb4da60ef2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

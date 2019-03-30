1344a6abb4a423b03de58a75d1218495f9965450
Alexander Dunn <alexander@alexanderontesting.com>
2019-03-30 13:47:47 +0000

    Copyright updates

--- src/Test/AlexanderOnTest.NetCoreWebDriverFactory.LinuxTests/RemoteWebDriverFactoryTests.cs
+++ src/Test/AlexanderOnTest.NetCoreWebDriverFactory.LinuxTests/RemoteWebDriverFactoryTests.cs
@@ -1,5 +1,5 @@
 // <copyright>
-// Copyright 2018 Alexander Dunn
+// Copyright 2019 Alexander Dunn
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
